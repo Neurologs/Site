d097f22566d4fc80102915c9a34688476ca77ed2
Neurologs <86210590+Neurologs@users.noreply.github.com>
2021-06-23 00:28:37 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -381,13 +381,14 @@ def page_tumors():
     with col1:
         st.write("")
         st.write("")
-        st.write("""
+        st.components.v1.html("""
         <p style="text-align:justify; font-size:18px; color:#3b4a46; font-weight:bold">Most common primary brain tumors</p>
         <p style="text-align:justify; font-size:14px; color:#3b4a46">Primary brain tumors are tumors that originate from the tissues of the brain or the brain’s immediate surroundings.</p>
         <p style="text-align:justify; font-size:14px; color:#3b4a46"><span style="color:red; font-weight:bold">Gliomas</span> arise from the gluey supportive cells of the brain («glia»). These are the most prevalent type of adult brain tumors, accounting for 78% of cancerous brain tumors.</p>
         <p style="text-align:justify; font-size:14px; color:#3b4a46"><span style="color:navy; font-weight:bold">Meningiomas</span> originate from the meninges. These are the most common benign intracranial tumors.</p>
         <p style="text-align:justify; font-size:14px; color:#3b4a46"><span style="color:green; font-weight:bold">Pituitary tumors</span> form in the pituitary gland. Most of these tumors are benign and slow-growing.</p>
-        """, unsafe_allow_html=True)
+        """, height=300)
+        
     with col2:
         st.write("")
         st.write("")
