555a20279db8cfd856d44181baf5b0e2b7ffecc4
Neurologs <86210590+Neurologs@users.noreply.github.com>
2021-06-23 10:14:21 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -454,15 +454,15 @@ def MMSE_page():
     st.markdown(title, unsafe_allow_html=True)
     
     st.markdown("""<style>
-                p {color:#126F90; font-weight:bold; text-align:justify}
+                p {color:#126F90; font-weight:bold; text-align:justify;}
                 li {color:#3b4a46; text-align:justify; font-weight:450}
                 .st-bq{color:#3A4044}
                 .time{color:#bb0f6e}
                 div.row-widget.stRadio > div{flex-direction:row;} .st-bg.st-bj.st-bk.st-bl.st-bm.st-bn.st-az.st-b4.st-bo.st-bp.st-bq.st-br.st-bs.st-bt.st-bu.st-bv.st-bw.st-bx.st-b2.st-by{background-color:#E2005B}
-                .css-1qpos38 {text-align:center; background-color:#12A67F; color:white; font-size:150%; width:445px; margin-right: 100px; margin-left: 100px; height:80px} 
+                .css-1qpos38 {text-align:center; background-color:#12A67F; color:white; font-size:150%; width:450px; height:60px} 
                 code, kbd {font-size: 120%; background-color:white; color: #bb0f6e;}
                 .css-1w0ubgf {background-color: lightyellow; height:250px}
-                .css-rncmk8.e1tzin5v0 p {text-align: center}
+                p .css-rncmk8.e1tzin5v0 {text-align: center}
                 </style>""", unsafe_allow_html=True)
     
     st.write("")
