6ff959e3c9140b3929ccb999325634277111d177
Neurologs <86210590+Neurologs@users.noreply.github.com>
2021-06-23 04:56:39 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -248,7 +248,7 @@ def homepage():
 def page_alzheimer():
      
     st.write("""<style>
-                .st-bq, .st-br {color:#10515C} .st-ag {font-weight: bold} .st-af {font-size: 1rem} .st-ek, .st-el {padding-left: 6px; padding-top:5px}                                        
+                .st-bq, .st-br {color:#10515C} .st-ag {font-weight: bold} .st-af {font-size: 1rem} .st-ek {padding-left: 6px; padding-top:5px}                                        
                 .st-e4 .st-e5 .st-e6, .st-e7, .st-d7, .st-d8, .st_d9, st-da {border-color:yellow}
                 .st-e0 {width: 30px} .st-e1 {height: 30px} .st-ea, .st-fo {background-color: #10515c} .st-eg {margin-left: 19px; margin-right: 14px;} 
                 .st-dx.st-b2.st-bp.st-dy.st-dz.st-e0.st-e1.st-e2.st-bc.st-bk.st-bl.st-bm.st-bn.st-bg.st-bh.st-bi.st-bj.st-e3.st-e4.st-e5.st-e6.st-av.st-aw.st-ax.st-ay.st-e7.st-cc.st-e8.st-e9.st-ea.st-eb.st-ec.st-ed.st-ee.st-c6.st-ef.st-eg
@@ -353,7 +353,7 @@ def page_alzheimer():
 def page_tumors():
         
     st.write("""<style>
-        .st-bq, .st-br {color:#10515C} .st-ag {font-weight: bold} .st-af {font-size: 1rem} .st-ek, .st-el {padding-left: 6px; padding-top:5px}                                        
+        .st-bq, .st-br {color:#10515C} .st-ag {font-weight: bold} .st-af {font-size: 1rem} .st-ek {padding-left: 6px; padding-top:5px}                                        
         .st-e4 .st-e5 .st-e6, .st-e7, .st-d7, .st-d8, .st_d9, st-da {border-color:yellow}
         .st-e0 {width: 30px} .st-e1 {height: 30px} .st-ea, .st-f0 {background-color: #10515c} .st-eg {margin-left: 19px; margin-right: 14px;} 
         .st-dx.st-b2.st-bp.st-dy.st-dz.st-e0.st-e1.st-e2.st-bc.st-bk.st-bl.st-bm.st-bn.st-bg.st-bh.st-bi.st-bj.st-e3.st-e4.st-e5.st-e6.st-av.st-aw.st-ax.st-ay.st-e7.st-cc.st-e8.st-e9.st-ea.st-eb.st-ec.st-ed.st-ee.st-c6.st-ef.st-eg
