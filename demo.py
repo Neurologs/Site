c27de4071e0e9c4f73cc5c43bc3cb1e400a7cfc4
Neurologs <86210590+Neurologs@users.noreply.github.com>
2021-09-08 13:31:48 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -220,7 +220,7 @@ def main():
 
     page = st.sidebar.selectbox("", tuple(pages.keys()))
     pages[page]()
-    st.markdown("""<style>.st-c5.st-bc.st-c6.st-c7.st-c8.st-be.st-c9.st-ca.st-cb, .css-1d0tddh.e1wbw4rs0, .st-br .st-bq {font-weight: bold; color: #26ca9f;} </style>""", unsafe_allow_html=True)
+    st.write("""<style>.st-br .st-bq {font-weight: bold; color: #26ca9f;}</style>""", unsafe_allow_html=True)
 
 # HOMEPAGE
 
