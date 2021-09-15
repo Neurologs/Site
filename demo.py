ecb1a19809bac62b1a969fee31cff3989e80b3db
Neurologs <86210590+Neurologs@users.noreply.github.com>
2021-09-15 11:07:30 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -366,10 +366,12 @@ def page_alzheimer():
 def page_tumors():
         
     st.write("""<style>
-    span.st-e1.st-e2.st-b2.st-bp.st-e2.st-e3.st-e4.st-e5.st-e6.st-bc.st-bk.st-bl.st-bm.st-bn.st-bg.st-bh.st-bi.st-bj.st-e7.st-e8.st-e9.st-ea.st-av.st-aw.st-ax.st-ay.st-eb.st-cc.st-ec.st-ed.st-ee.st-ef.st-eg.st-eh.st-ei.st-c6.st-ej.st-ek
-    {width: 30px; height: 30px; border-color: yellow; margin-left: 18px; margin-right: 10px; background-color: rgb(16 81 92); transition-property: none;} 
-    span.st-e1.st-e2.st-b2.st-bp.st-e2.st-e3.st-e4.st-e5.st-e6.st-bc.st-bk.st-bl.st-bm.st-bn.st-bg.st-bh.st-bi.st-bj.st-d7.st-d8.st-d9.st-da.st-av.st-aw.st-ax.st-ay.st-eb.st-cc.st-ec.st-eq.st-er.st-ee.st-ef.st-eg.st-eh.st-ei.st-c6.st-ej.st-ek 
-    {width: 30px; height: 30px; border-color: yellow; margin-left: 18px; margin-right: 10px; background-color: rgb(16 81 92); transition-property: none;} 
+    .st-bq, .st-br {color:#10515C} .st-ag {font-weight: bold} .st-af {font-size: 1rem}                                    
+    .st-e4 .st-e5 .st-e6, .st-e7, .st-d7, .st-d8, .st_d9, st-da {border-color:yellow}
+    .st-dx.st-b2.st-bp.st-dy.st-dz.st-e0.st-e1.st-e2.st-bc.st-bk.st-bl.st-bm.st-bn.st-bg.st-bh.st-bi.st-bj.st-e3.st-e4.st-e5.st-e6.st-av.st-aw.st-ax.st-ay.st-e7.st-cc.st-e8.st-e9.st-ea.st-eb.st-ec.st-ed.st-ee.st-c6.st-ef.st-eg
+    {border-color: yellow; height: 30px; width: 30px; border-width: 3px; transition-property: none;}
+    .st-e1 {height: 30px; width: 30px; padding-left: 6px; padding-top:5px; margin-left: 18px; margin-right: 10px;} 
+    .st-er {background-color: rgb(16 81 92)}
     svg.st-cc.st-d2.st-bq.st-d3.st-d4.st-d5 {color: yellow}
     .css-9ycgxx.exg6vvm3 {color:white}
     .css-113fe4q.euu6i2w0 {color:gray}
