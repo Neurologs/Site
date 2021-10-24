6bfe858718dfabc5bd6bccdd248050a415c5f529
Neurologs <86210590+Neurologs@users.noreply.github.com>
2021-10-24 08:52:28 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -747,12 +747,12 @@ def page_tumors():
             plt.axis('off')
             st.pyplot(fig2)
             
-            if interactive_view:
-                html_view = nlplt.view_img(niimg, bg_img=False, colorbar=False, threshold='auto', black_bg=True, cmap='binary')
-                html_view.save_as_html('viewer.html')
-                HtmlFile = open("viewer.html", 'r', encoding='utf-8')
-                source_code = HtmlFile.read() 
-                components.html(source_code)
+        if interactive_view:
+            html_view = nlplt.view_img(niimg, bg_img=False, colorbar=False, threshold='auto', black_bg=True)
+            html_view.save_as_html('viewer.html')
+            HtmlFile = open("viewer.html", 'r', encoding='utf-8')
+            source_code = HtmlFile.read() 
+            components.html(source_code)
     
         st.write('')
         st.write('')
