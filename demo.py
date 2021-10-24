3cbc3d12448626a95d764bd0b24e6e999bad7741
Neurologs <86210590+Neurologs@users.noreply.github.com>
2021-10-24 07:19:00 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -260,6 +260,7 @@ def page_alzheimer():
                 {border-color: yellow; height: 30px; width: 30px; border-width: 3px; background-color: #555867;}
                 .st-e1 {height: 30px; width: 30px; padding-left: 6px; padding-top:5px; margin-left: 18px; margin-right: 10px;}
                 .st-eo {background-color: rgb(16 81 92);}
+                .st-ek {line-height: 30px;}
                 .css-9ycgxx.exg6vvm3 {color:white}
                 .css-113fe4q.euu6i2w0 {color:gray}
                 .css-1op0mqd.exg6vvm2 {color:yellow} 
@@ -610,6 +611,7 @@ def page_tumors():
     {border-color: yellow; height: 30px; width: 30px; border-width: 3px; background-color: #555867;}
     .st-e1 {height: 30px; width: 30px; padding-left: 6px; padding-top:5px; margin-left: 18px; margin-right: 10px;} 
     .st-eo {background-color: rgb(16 81 92);}
+    .st-ek {line-height: 30px;}
      svg.st-cc.st-d2.st-bq.st-d3.st-d4.st-d5 {color: yellow}
     .css-9ycgxx.exg6vvm3 {color:white}
     .css-113fe4q.euu6i2w0 {color:gray}
