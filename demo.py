e9471c86ddca1be3aefab031cbed8eabd2491f24
Neurologs <86210590+Neurologs@users.noreply.github.com>
2021-11-09 22:55:02 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -261,6 +261,8 @@ def page_alzheimer():
                 {border-color: yellow; height: 30px; width: 30px; border-width: 3px; background-color: #555867;}
                 span.st-dx.st-b2.st-bp.st-dy.st-dz.st-e0.st-e1.st-e2.st-bc.st-bk.st-bl.st-bm.st-bn.st-bg.st-bh.st-bi.st-bj.st-d7.st-d8.st-d9.st-da.st-av.st-aw.st-ax.st-ay.st-e7.st-cc.st-e8.st-ez.st-eo.st-ea.st-eb.st-ec.st-ed.st-ee.st-c6.st-ef.st-eg 
                 {border-color: yellow; height: 30px; width: 30px; border-width: 3px; background-color: #12A67F;}
+                span.st-e1.st-b2.st-bp.st-e2.st-e3.st-e4.st-e5.st-e6.st-bc.st-bk.st-bl.st-bm.st-bn.st-bg.st-bh.st-bi.st-bj.st-d7.st-d8.st-d9.st-da.st-av.st-aw.st-ax.st-ay.st-eb.st-cc.st-ec.st-eq.st-er.st-ee.st-ef.st-eg.st-eh.st-ei.st-c6.st-ej.st-ek 
+                {border-color: yellow; height: 30px; width: 30px; border-width: 3px; background-color: #12A67F;}
                 .st-e1 {height: 30px; width: 30px; padding-left: 6px; padding-top:5px; margin-left: 18px; margin-right: 10px;}
                 .st-ek {line-height: 30px;}
                 .css-9ycgxx.exg6vvm3 {color:white}
