691958d7f09200479214786ac325b3c713e5346b
Neurologs <86210590+Neurologs@users.noreply.github.com>
2021-11-09 20:54:13 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -260,6 +260,7 @@ def page_alzheimer():
                 {border-color: yellow; height: 30px; width: 30px; border-width: 3px; background-color: #555867;}
                 .st-e1 {height: 30px; width: 30px; padding-left: 6px; padding-top:5px; margin-left: 18px; margin-right: 10px;}
                 .st-ek {line-height: 30px;}
+                .st-eo {background-color: #12A67F}
                 .css-9ycgxx.exg6vvm3 {color:white}
                 .css-113fe4q.euu6i2w0 {color:gray}
                 .css-1op0mqd.exg6vvm2 {color:yellow} 
